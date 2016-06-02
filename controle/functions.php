f8e0fa90e621329ec3849e456aea39f260a3583c
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-02 01:52:53 -0300

    Adicionado menus do sistema

--- controle/functions.php
+++ controle/functions.php
@@ -1,6 +1,22 @@
 <?php 
 	function incluir_menu(){
-		include_once(TEMPLATES.'/geral/menu.php');
+		if(!isset($_SESSION['idpessoa'])){
+			include_once(TEMPLATES.'/geral/menu.php');
+		}
+		else if(isset($_SESSION['idpessoa'])){
+			if(isset($_SESSION['idpaciente'])){
+				include_once(TEMPLATES.'/geral/menu_paciente.php');
+			}
+			if(isset($_SESSION['idmedico'])){
+				include_once(TEMPLATES.'/geral/menu_medico.php');
+			}
+			if(isset($_SESSION['idrecepcionista'])){
+				include_once(TEMPLATES.'/geral/menu_recepcionista.php');
+			}
+			if(isset($_SESSION['idadministrador'])){
+				include_once(TEMPLATES.'/geral/menu_administrador.php');
+			}
+		}
 	}
 
 	function swal($title = '', $text = '', $type = '', $location = '', $btn = 'btn-primary'){
---
+++ templates/geral/menu_administrador.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-default navbar-static-top" role='navigation'>
+    <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/<?php echo BASE; ?>">Página inicial</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div id='navbar' class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="/<?php echo BASE; ?>admin">PHPMyAdmin</a></li>
+                <li class='dropdown'>
+                    <a href="#" class='dropdown-toggle' data-toggle='dropdown'>Menu Administrador<span class="caret"></span></a>
+                    <ul class='dropdown-menu'>
+                        <li><a href="<?php echo ADMIN; ?>listar_hospitais">Item 1</a></li>
+                        <li><a href="<?php echo ADMIN; ?>cadastrar_hospital">Item 2</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-submenu">
+                            <a tabindex="-1" href="#">Submenu<div class='inline'><i class="fa fa-caret-right" style=''></i></div></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Subitem 1</a></li>
+                                <li><a href="#">Subitem 2</a></li>
+                            </ul>
+                        </li>
+                    </ul> 
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">                
+                <li class='dropdown'>
+                    <a href="#" class='dropdown-toggle' data-toggle='dropdown'><i class="fa fa-cog"></i>&nbsp;Opções<span class="caret"></span></a>
+                    <ul class='dropdown-menu'>
+                        <li><a href="<?php echo ADMIN; ?>meus_dados">Meus dados</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#" <?php echo "onclick='log_out(\"".BASE."\")'"; ?>>Sair</a></li>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
