32d374cc57075e22ff5ebb1c8c157d8cafba2cd4
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-02 01:52:08 -0300

    Banco de dados atualizado

--- controle/criar_banco.sql
+++
@@ -1,4 +0,0 @@
-
-drop database ;
-create database  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-USE ;
---
+++ controle/sql_bd.sql
@@ -0,0 +1,258 @@
+drop database CliniSoftware;
+CREATE database CliniSoftware;
+USE CliniSoftware;
+
+create table pessoa(
+	idpessoa int not null auto_increment,
+	telefone varchar(15) not null,
+    cpf varchar(14),
+    data_nascimento date not null,
+    email varchar(45),
+    pnome varchar(45) not null,
+	mnome varchar(45),
+	unome varchar(45) not null,
+    
+    primary key(idpessoa),
+    unique(cpf),
+    unique(email)
+
+);
+
+create table usuario(
+	idusuario int not null auto_increment,
+	senha varchar(15) not null,
+	idpessoa int not null,
+	
+	primary key (idusuario),
+	foreign key (idpessoa) references pessoa(idpessoa) 
+	
+);
+
+create table funcionario(
+	idfuncionario int not null auto_increment,
+    idpessoa int not null,
+    salario float not null,
+    data_pagamento date not null,
+    
+    primary key(idfuncionario),
+    foreign key(idpessoa) references pessoa(idpessoa)
+
+);
+
+create table expediente(
+	idexpediente int not null auto_increment,
+    hora_inicial_expediente varchar(5) not null,
+	hora_final_expediente varchar(5) not null,
+	hora_inicial_intervalo varchar(5) not null,
+	hora_final_intervalo varchar(5) not null,
+    dia_semana varchar(13) not null,
+    
+    primary key (idexpediente)
+    
+);
+
+create table trabalha(
+	idtrabalha int not null auto_increment,
+    idexpediente int not null,
+    idfuncionario int not null,
+    
+    primary key (idtrabalha),
+    foreign key (idexpediente) references expediente(idexpediente),
+    foreign key (idfuncionario) references funcionario(idfuncionario)
+
+);
+
+create table administrador(
+	idadministrador int not null auto_increment,
+    idfuncionario int not null,
+    
+    primary key(idadministrador),
+    foreign key(idfuncionario) references funcionario(idfuncionario)
+
+);
+
+create table recepcionista(
+	idrecepcionista int not null auto_increment,
+	idfuncionario int not null,
+     
+    primary key(idrecepcionista),
+    foreign key(idfuncionario) references funcionario(idfuncionario)
+ 
+);
+
+create table medico(
+	idmedico int not null auto_increment,
+    idfuncionario int not null,
+    preco_padrao float,
+    cadastro_unico int not null,
+    
+    primary key(idmedico),
+    foreign key(idfuncionario) references funcionario(idfuncionario)
+
+);
+
+create table paciente(
+	idpaciente int not null auto_increment,
+    idpessoa int not null,
+    
+    primary key(idpaciente),
+    foreign key(idpessoa) references pessoa(idpessoa)
+
+);
+
+create table gerencia(
+	idgerencia int not null auto_increment,
+    idrecepcionista int not null,
+    idmedico int not null,
+    
+    primary key(idgerencia),
+    foreign key(idrecepcionista) references recepcionista(idrecepcionista),
+    foreign key(idmedico) references medico(idmedico)
+
+);
+
+create table especialiadade(
+	idespecialidade int not null auto_increment,
+    nome varchar(45) not null,
+
+	primary key(idespecialidade)
+);
+
+create table especializado(
+	idespecializado int not null auto_increment,
+    idmedico int not null,
+    idespecialidade int not null,
+    
+	primary key(idespecializado),
+    foreign key(idmedico) references medico(idmedico),
+    foreign key(idespecialidade) references especialiadade(idespecialidade)
+);
+
+
+create table mercancia(
+	idmercancia int not null auto_increment,
+
+	primary key(idmercancia)
+);
+
+
+create table consulta(
+	idconsulta int not null auto_increment,
+    idmercancia int not null,
+    hora varchar(5) not null,
+    data date not null,
+    preco float not null,
+    
+
+	primary key(idconsulta),
+    foreign key(idmercancia) references mercancia(idmercancia)
+);
+
+create table ocorrencia_medica(
+	idocorrencia_medica int not null auto_increment,
+    data date not null,
+    diagnostico varchar(45) not null,
+
+	primary key(idocorrencia_medica)
+);
+
+create table tem(
+	idtem int not null auto_increment,
+    idocorrencia_medica int not null,
+    idpaciente int not null,
+
+	primary key(idtem),
+    foreign key(idocorrencia_medica) references ocorrencia_medica(idocorrencia_medica),
+    foreign key(idpaciente) references paciente(idpaciente)
+);
+
+create table conta(
+	idconta int not null auto_increment,
+	idpaciente int not null,
+	pago boolean not null,
+
+	primary key(idconta),
+	foreign key(idpaciente) references paciente(idpaciente)
+);
+
+
+create table cobranca(
+	idcobranca int not null auto_increment,
+	idconta int not null,
+	idmercancia int not null,
+
+	primary key(idcobranca),
+	foreign key(idconta) references conta(idconta),
+	foreign key(idmercancia) references mercancia(idmercancia)
+);
+
+create table procedimento(
+	idprocedimento int not null auto_increment,
+	idmercancia int not null,
+	nome varchar(45) not null,
+	preco float not null,
+	
+	primary key(idprocedimento),
+	foreign key(idmercancia) references mercancia(idmercancia)
+);
+
+create table produto(
+	idproduto int not null auto_increment,
+	idmercancia int not null,
+	nome varchar(45) not null,
+	preco float not null,
+
+	primary key(idproduto),
+	foreign key(idmercancia) references mercancia(idmercancia)
+);
+
+create table inclui(
+	idinclui int not null auto_increment,
+	idproduto int not null,
+	idprocedimento int not null,
+
+	primary key(idinclui),
+	foreign key(idproduto) references produto(idproduto),
+	foreign key(idprocedimento) references procedimento(idprocedimento)
+);
+
+create table agenda(
+	idagenda int not null auto_increment,
+	idpaciente int not null,
+	idmedico int not null,
+	idconsulta int not null,
+
+	primary key(idagenda),
+	foreign key(idpaciente) references paciente(idpaciente),
+	foreign key(idmedico) references medico(idmedico),
+	foreign key(idconsulta) references consulta(idconsulta)
+);
+
+insert into pessoa (telefone, cpf, data_nascimento, email, pnome, mnome, unome) values 
+	('(84) 99818-4097', '016.887.454-75', '1994-12-27', 'rodrigondec@gmail.com', 'Rodrigo', 'Nunes', 'de Castro'),
+	('(84) 99166-5652', null, '1994-12-26', 'fernandachaconfontoura@gmail.com', 'Fernanda', 'Chacon', 'Fountoura'),
+	('(84) 11111-1111', null, '1994-12-25', 'major@gmail.com', 'Major', 'Fulano', 'de Testes'),
+	('(84) 22222-2222', null, '1994-12-24', 'marcel@gmail.com', 'Marcel', 'Professor', 'Soberano'),
+	('(84) 333333-3333', null, '1994-12-23', 'francisco@gmail.com', 'francisco', 'Monitor', 'de Tal');
+
+insert into usuario (senha, idpessoa) values 
+	('rodrigo', 1),
+	('fernanda', 2),
+	('major', 3),
+	('marcel', 4),
+	('francisco', 5);
+
+insert into funcionario (idpessoa, salario, data_pagamento) values
+	(1, 800.00, '05'),
+	(2, 700.00, '05'),
+	(3, 900.00, '05');
+
+insert into administrador (idfuncionario) values (1);
+
+insert into recepcionista (idfuncionario) values (3);
+
+insert into medico (idfuncionario) values (2);
+
+insert into gerencia (idrecepcionista, idmedico) values (1, 1);
+
+insert into paciente (idpessoa) values (4);
