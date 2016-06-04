8b4a37da27a23dac8527071d6795d3526b05c92a
fernandachacon <fernandachaconfontoura@gmail.com>
2016-06-04 14:20:38 -0300

    Formulário de cadastrar um médico. Ainda vou fazer algumas alterações

--- templates/administrador/cadastrar_medico.php
+++ templates/administrador/cadastrar_medico.php
@@ -1,5 +1,60 @@
-<div class='text-center'>
-	<h2>Cadastrar Médico</h2>
-	<hr />
+
+<div class="container" style="padding-left: 216px;">
+            <div class="row" style="
+    padding-right: 130px;
+">
+				<div class='text-center'>
+					<h2>Cadastrar Médico</h2>
+					<hr />
+				</div>
+			</div>
+
+			<div class="row">
+ 				<div class="form-group col-md-6" style="padding-left: 83px;">
+   					<label for="campo1">Nome Completo</label>
+   					<input type="text" class="form-control" id="campo1">
+ 				</div>
+
+ 				<div class="form-group col-md-3">
+  	 				<label for="campo2">CPF</label>
+   					<input type="text" class="form-control" id="campo3">
+ 				</div>
+ 
+			</div>
+			<div class="row">
+
+				<div class="form-group col-md-4" style="padding-left: 83px;padding-right: 32px;">
+   					<label for="campo1">Data de Nascimento</label>
+   					<input type="text" class="form-control" id="campo1">
+ 				</div>
+
+				<div class="form-group col-md-3" style="padding-left: 3px;">
+   					<label for="campo1">Cadastro Único</label>
+   					<input type="text" class="form-control" id="campo1">
+ 				</div>
+
+ 				<div class="form-group col-md-2">
+   					<label for="campo1">Preço Padrão</label>
+   					<input type="text" class="form-control" id="campo1">
+ 				</div>
+
+			</div>
+
+			<div class="row">
+				<div class="form-group col-md-5" style="padding-left: 69px;"">
+   					<label for="campo1">Email</label>
+   					<input type="text" class="form-control" id="campo1">
+ 				</div>
+
+ 				<div class="form-group col-md-3">
+   					<label for="campo1">Telefone</label>
+   					<input type="text" class="form-control" id="campo1">
+ 				</div>
+
+			</div>
+
+			<div class="row" style="margin-left: 608px;">
+				<button type="submit" class="btn btn-primary">Cadastrar</button>
+			</div>
+
 </div>
-eertyhuik
