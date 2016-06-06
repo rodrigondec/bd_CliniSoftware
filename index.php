207ede346c1b98847a9f62e6aa030e612ab5e467
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-06 16:23:28 -0300

    MEU CSS É MELHOR!!!!

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
 
         <!-- Latest compiled and minified CSS -->
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-
         
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="/<?php echo BASE; ?>estaticos/bootstrap-sweetalert/lib/sweet-alert.css">
-        <link rel="stylesheet" href="/<?php echo BASE; ?>estaticos/estilo.css">  
         <link rel="stylesheet" href="/<?php echo BASE; ?>estaticos/bootstrap-fernanda/css/freelancer.css">  
         <link rel="stylesheet" href="/<?php echo BASE; ?>estaticos/bootstrap-fernanda/css/bootstrap.css"> 
         <link rel="stylesheet" href="/<?php echo BASE; ?>estaticos/bootstrap-fernanda/css/freelancer.min.css"> 
+
+        <link rel="stylesheet" href="/<?php echo BASE; ?>estaticos/estilo.css">  
 
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
