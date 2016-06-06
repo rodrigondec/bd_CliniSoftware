ff57ab2243d7007177e0421a79de209968266bb2
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-06 16:00:25 -0300

    formulário do cadastrar medico ok

--- templates/administrador/cadastrar_medico.php
+++ templates/administrador/cadastrar_medico.php
@@ -1,60 +1,36 @@
-
-<div class="container" style="padding-left: 216px;">
-            <div class="row" style="
-    padding-right: 130px;
-">
-				<div class='text-center'>
-					<h2>Cadastrar Médico</h2>
-					<hr />
-				</div>
-			</div>
-
-			<div class="row">
- 				<div class="form-group col-md-6" style="padding-left: 83px;">
-   					<label for="campo1">Nome Completo</label>
-   					<input type="text" class="form-control" id="campo1">
- 				</div>
-
- 				<div class="form-group col-md-3">
-  	 				<label for="campo2">CPF</label>
-   					<input type="text" class="form-control" id="campo3">
- 				</div>
- 
-			</div>
-			<div class="row">
-
-				<div class="form-group col-md-4" style="padding-left: 83px;padding-right: 32px;">
-   					<label for="campo1">Data de Nascimento</label>
-   					<input type="text" class="form-control" id="campo1">
- 				</div>
-
-				<div class="form-group col-md-3" style="padding-left: 3px;">
-   					<label for="campo1">Cadastro Único</label>
-   					<input type="text" class="form-control" id="campo1">
- 				</div>
-
- 				<div class="form-group col-md-2">
-   					<label for="campo1">Preço Padrão</label>
-   					<input type="text" class="form-control" id="campo1">
- 				</div>
-
-			</div>
-
-			<div class="row">
-				<div class="form-group col-md-5" style="padding-left: 69px;"">
-   					<label for="campo1">Email</label>
-   					<input type="text" class="form-control" id="campo1">
- 				</div>
-
- 				<div class="form-group col-md-3">
-   					<label for="campo1">Telefone</label>
-   					<input type="text" class="form-control" id="campo1">
- 				</div>
-
-			</div>
-
-			<div class="row" style="margin-left: 608px;">
-				<button type="submit" class="btn btn-primary">Cadastrar</button>
-			</div>
-
+<div class='container col-lg-5 center'>
+	<form method='post'>
+		<div class='form-group col-lg-12'>
+		    <label for=''>Nome</label>
+		    <input type='text' name='' class='form-control' placeholder=''  required />
+		</div>
+		<div class='form-group col-lg-7'>
+		    <label for=''>CPF</label>
+		    <input type='text' name='' class='form-control' placeholder=''  required />
+		</div>
+		<div class='form-group col-lg-5'>
+		    <label for=''>Data de Nascimento</label>
+		    <input type='text' name='' class='form-control' placeholder=''  required />
+		</div>
+		<div class='form-group col-lg-7'>
+		    <label for=''>Cadastro Único</label>
+		    <input type='text' name='' class='form-control' placeholder=''  required />
+		</div>
+		<div class='form-group col-lg-5'>
+		    <label for=''>Preço Padrão</label>
+		    <input type='text' name='' class='form-control' placeholder=''  required />
+		</div>
+		<div class='form-group col-lg-6'>
+		    <label for=''>Email</label>
+		    <input type='text' name='' class='form-control' placeholder=''  required />
+		</div>
+		<div class='form-group  col-lg-6'>
+		    <label for=''>Telefone</label>
+		    <input type='text' name='' class='form-control' placeholder=''  required />
+		</div>
+		<div class='text-center'>
+			<button class='btn btn-danger' type='reset'>Cadastrar</button>
+            <button class='btn btn-primary' type='submit'>Enviar</button>
+		</div>
+	</form>
 </div>
