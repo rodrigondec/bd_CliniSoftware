63e77f37284b72982c003a79891342133e11a9d2
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-09 02:21:34 -0300

    erro de portugues corrigido

--- templates/sistema/home.php
+++ templates/sistema/home.php
@@ -59,7 +59,7 @@
                 <div class="col-lg-8 col-lg-offset-2 text-center">
                     <p>CliniSoftware é um sistema de gerenciamento de clínicas, em que paciente, médicos e recepcionistas possuem acesso, cada uma com suas atividades específicas.
                     O software permite a marcação de consulta, assim como desmarcar, é possível visualizar as consultas de uma paciente e de um médico. O médicos salvam no sistema
-                    o histórico de cada paciente. Essa são só algumas das atividade feitas no CliniSoftware. Para saber mais entre em contato conosco e responderemos em breve.</p>
+                    o histórico de cada paciente. Essas são só algumas das atividade feitas no CliniSoftware. Para saber mais entre em contato conosco e responderemos em breve.</p>
                 </div>
 
                
