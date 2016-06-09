353ffe9a2658098fb6a6754e0153d1c54b27338b
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-09 02:08:04 -0300

    tabela identada e com classes diferentes do bootstrap

--- templates/administrador/listar_medicos.php
+++ templates/administrador/listar_medicos.php
@@ -3,29 +3,29 @@
 	<hr />
 </div>
 
-<div class="container-fluid">         
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Nome</th>
-        <th>Email</th>
-        <th>CPF</th>
-        <th>Cadastro Único</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Fernanda Chacon Fontoura</td>
-        <td>fernandachaconfontoura@gmail.com</td>
-        <td>103.918.374-35</td>
-        <td>123456789/000</td>
-      </tr>
-      <tr>
-        
-      </tr>
-      <tr>
-        
-      </tr>
-    </tbody>
-  </table>
+<div class="table-responsive container">
+    <table class="table table-striped table-hover table-condensed">
+        <thead> 
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>CPF</th>
+                <th>Cadastro Único</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Fernanda Chacon Fontoura</td>
+                <td>fernandachaconfontoura@gmail.com</td>
+                <td>103.918.374-35</td>
+                <td>123456789/000</td>
+            </tr>
+            <tr>
+
+            </tr>
+            <tr>
+
+            </tr>
+        </tbody>
+    </table>
 </div>
