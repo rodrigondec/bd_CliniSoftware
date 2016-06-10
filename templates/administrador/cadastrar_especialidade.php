f3ffb402501cc32fa55eb9a3ae474afd61e37151
fernandachacon <fernandachaconfontoura@gmail.com>
2016-06-10 15:43:51 -0300

    form cadastrar especialidade

--- templates/administrador/cadastrar_especialidade.php
+++ templates/administrador/cadastrar_especialidade.php
@@ -2,3 +2,17 @@
 	<h2>Cadastrar Especialidade</h2>
 	<hr />
 </div>
+
+<div class='container col-lg-5 center'>
+	<form method='post'>
+		<div class='form-group col-lg-12'>
+		    <label for=''>Nome</label>
+		    <input type='text' name='nome' class='form-control' placeholder='Digite aqui a especialidade.'  required />
+		</div>
+
+		<div class='text-center'>
+			<button class='btn btn-danger' type='reset'>Apagar</button>
+            <button class='btn btn-primary' type='submit'>Cadastrar</button>
+		</div>
+	</form>
+</div>
