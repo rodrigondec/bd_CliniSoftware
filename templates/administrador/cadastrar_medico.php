b9c77507c1d8fe65324499c32a07353908e2d60f
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-13 04:40:21 -0300

    cadastrar medico funcionando

--- templates/administrador/cadastrar_medico.php
+++ templates/administrador/cadastrar_medico.php
@@ -57,16 +57,69 @@
 </div>
 <?php  
 	if(count($_POST) > 0){
+		var_dump($_POST); echo '<br><br>';
 		/* CADASTRO DE PESSOA OU VERIFICAÇÃO */
-		$pessoa = select('*', 'pessoa', 'email', $_POST['email']);
-		var_dump($pessoa);
+		$pessoa = run_select('select * from pessoa where email=\''.$_POST['email'].'\';');
+		if($pessoa){ 
+			/* PESSOA JÁ EXISTE */
+			var_dump($pessoa);
 
-		if($pessoa){ /* PESSOA EXISTE */
+			$funcionario = run_select('select * from funcionario where idpessoa='.$pessoa['idpessoa'].';');
+			if(!$funcionario){
+				$funcionario = array();
+				$funcionario['idpessoa'] = $pessoa['idpessoa'];
+				$funcionario['salario'] = $_POST['salario'];
+				$funcionario['data_pagamento'] = $_POST['data_pagamento'];
 
+				insert($funcionario, 'funcionario');
+				$funcionario['idfuncionario'] = run_select('select max(idfuncionario) from funcionario;')['max(idfuncionario)'];
+			}
+			
+			$medico = run_select('select * from medico where idfuncionario='.$funcionario['idfuncionario'].';');
+			if(!$medico){
+				$medico = array();
+				$medico['preco_padrao'] = $_POST['preco_padrao'];
+				$medico['cadastro_unico'] = $_POST['cadastro_unico'];
+				$medico['idfuncionario'] = $funcionario['idfuncionario'];
+
+				insert($medico, 'medico');
+
+				ob_clean();
+				header('LOCATION: /'.BASE);
+			}
+			else{
+				swal('Erro!', 'Essa pessoa já está cadastrada como um médico.', 'error', ADMINISTRADOR.'/cadastrar_medico', 'btn-danger');
+			}
 		}
 		else{
+			/* CADASTRO PESSOA */
 			$pessoa = array();
-			$pessoa['nome'] = $_POST['nome']
+			$pessoa['nome'] = $_POST['nome'];
+			$pessoa['data_nascimento'] = $_POST['data_nascimento'];
+			$pessoa['telefone'] = $_POST['telefone'];
+			$pessoa['email'] = $_POST['email'];
+			$pessoa['cpf'] = $_POST['cpf'];
+
+			insert($pessoa, 'pessoa');
+			$idpessoa = run_select('select max(idpessoa) from pessoa;');
+
+			$funcionario = array();
+			$funcionario['idpessoa'] = $idpessoa['max(idpessoa)'];
+			$funcionario['salario'] = $_POST['salario'];
+			$funcionario['data_pagamento'] = $_POST['data_pagamento'];
+
+			insert($funcionario, 'funcionario');
+			$idfuncionario = run_select('select max(idfuncionario) from funcionario;');
+
+			$medico = array();
+			$medico['preco_padrao'] = $_POST['preco_padrao'];
+			$medico['cadastro_unico'] = $_POST['cadastro_unico'];
+			$medico['idfuncionario'] = $idfuncionario['max(idfuncionario)'];
+
+			insert($medico, 'medico');
+
+			ob_clean();
+			header('LOCATION: /'.BASE);
 		}
 	}
 ?>
