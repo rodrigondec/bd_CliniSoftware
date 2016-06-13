84536c4b9c664c0e57dedc84dff40fb27fcbfe0f
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-13 17:21:55 -0300

    invalidar medico funcionando

--- templates/administrador/invalidar_medico.php
+++ templates/administrador/invalidar_medico.php
@@ -1,4 +1,8 @@
-<div class='text-center'>
-	<h2>Invalidar Médico</h2>
-	<hr />
-</div>
+<?php  
+	if(isset($_GET['idmedico'])){
+		$dados['ativo'] = 0;
+		update($dados, 'medico', 'idmedico', $_GET['idmedico']);
+		ob_clean();
+        header('LOCATION: '.ADMINISTRADOR.'/listar_medicos');
+	}
+?>
