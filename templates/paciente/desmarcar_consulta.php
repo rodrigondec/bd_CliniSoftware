8259263a94048157a6f7328930e63e362c1f8814
fernandachacon <fernandachaconfontoura@gmail.com>
2016-06-13 17:32:49 -0300

    tela de desmarcar consulta

---
+++ templates/paciente/desmarcar_consulta.php
@@ -0,0 +1,23 @@
+<div class='text-center'>
+	<h2>Desmarcar Consulta</h2>
+	<hr />
+</div>
+
+<div class='container col-lg-5 center'>
+	<form method='post' >
+		<div class='form-group col-lg-12'>
+		    <label for=''>Consultas Marcadas: </label>
+		    <input type='text' name='nome' class='form-control' placeholder='Digite a especialidade que deseja marcar a consulta'  required />
+		</div>
+		
+		<div class='text-center'>
+			<button class='btn btn-danger' type='reset'>Apagar</button>
+            <button class='btn btn-primary' type='submit'>Buscar médicos</button>
+		</div>
+	</form>
+</div>
+<?php  
+	
+
+
+?>
