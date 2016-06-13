c558400c4962a44cd264ef1e0e7a5f406b262f85
fernandachacon <fernandachaconfontoura@gmail.com>
2016-06-13 17:33:46 -0300

    tela de desmarcar consulta na área do recepcionista

--- templates/recepcionista/desmarcar_consulta.php
+++ templates/recepcionista/desmarcar_consulta.php
@@ -1,4 +1,30 @@
 <div class='text-center'>
 	<h2>Desmarcar Consulta</h2>
-	<hr />
 </div>
+
+<div class="table-responsive container">
+    <table class="table table-striped table-hover table-condensed">
+	<form >
+		<div class='form-group col-lg-12'>
+		    <label for=''>Consultas Marcadas</label>
+		    <br />
+		    <br />
+		 
+		    <thead> 
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>CPF</th>
+                <th>Cadastro Único</th>
+                <th class='col-lg-1'></th>
+                <th class='col-lg-1'></th>
+                
+            </tr>
+        	</thead>
+        	</div>
+     </table>
+		    
+	
+	</form>
+</div>
+        
