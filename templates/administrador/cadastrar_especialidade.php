5707604651102c3593eb06ffe1d1789904a9fdf1
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-13 17:41:54 -0300

    alterar especialidade funcionanando

--- templates/administrador/cadastrar_especialidade.php
+++ templates/administrador/cadastrar_especialidade.php
@@ -2,18 +2,22 @@
 	<h2>Cadastrar Especialidade</h2>
 	<hr />
 </div>
-
 <div class='container col-lg-5 center'>
 	<form method='post'>
 		<div class='form-group col-lg-12'>
-		    <label for=''>Nome</label>
+		    <label for='nome'>Nome</label>
 		    <input type='text' name='nome' class='form-control' placeholder='Digite aqui a especialidade.'  required />
 		</div>
-
 		<div class='text-center'>
 			<button class='btn btn-danger' type='reset'>Apagar</button>
             <button class='btn btn-primary' type='submit'>Cadastrar</button>
 		</div>
 	</form>
 </div>
-
+<?php  
+	if(count($_POST) > 0){
+		insert($_POST, 'especialidade');
+		ob_clean();
+		header('LOCATION: '.ADMINISTRADOR.'/listar_especialidades');
+	}
+?>
