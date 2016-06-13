e11573b844f5555c3eae643f8c50258c3452129f
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-13 04:38:14 -0300

    id's podem ser null para serem setados null

--- controle/criar_banco.sql
+++ controle/criar_banco.sql
@@ -127,35 +127,37 @@ create table consulta(
 
 create table ocorrencia_medica(
 	idocorrencia_medica int not null auto_increment,
+	idpaciente int not null,
     data date not null,
     diagnostico varchar(45) not null,
-	primary key(idocorrencia_medica)
+	primary key(idocorrencia_medica),
+	foreign key(idpaciente) references paciente(idpaciente) on delete cascade
 );
 
-create table tem(
+/*create table tem(
 	idtem int not null auto_increment,
     idocorrencia_medica int not null,
     idpaciente int not null,
 	primary key(idtem),
     foreign key(idocorrencia_medica) references ocorrencia_medica(idocorrencia_medica),
     foreign key(idpaciente) references paciente(idpaciente)
-);
+);*/
 
 create table conta(
 	idconta int not null auto_increment,
-	idpaciente int not null,
+	idpaciente int,
 	pago boolean not null,
 	primary key(idconta),
-	foreign key(idpaciente) references paciente(idpaciente)
+	foreign key(idpaciente) references paciente(idpaciente) on delete set null
 );
 
 
 create table cobranca(
-	idcobranca int not null auto_increment,
+	-- idcobranca int not null auto_increment,
 	idconta int not null,
 	idmercancia int not null,
-	primary key(idcobranca),
-	foreign key(idconta) references conta(idconta),
+	-- primary key(idcobranca),
+	foreign key(idconta) references conta(idconta) on delete cascade,
 	foreign key(idmercancia) references mercancia(idmercancia)
 );
 
