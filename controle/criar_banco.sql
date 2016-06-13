a5ad8591fffb39eab6bcdb093579d553efc74eb9
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-13 15:42:25 -0300

    Removido entidade Tem

--- controle/criar_banco.sql
+++ controle/criar_banco.sql
@@ -134,15 +134,6 @@ create table ocorrencia_medica(
 	foreign key(idpaciente) references paciente(idpaciente) on delete cascade
 );
 
-/*create table tem(
-	idtem int not null auto_increment,
-    idocorrencia_medica int not null,
-    idpaciente int not null,
-	primary key(idtem),
-    foreign key(idocorrencia_medica) references ocorrencia_medica(idocorrencia_medica),
-    foreign key(idpaciente) references paciente(idpaciente)
-);*/
-
 create table conta(
 	idconta int not null auto_increment,
 	idpaciente int,
--- index.php
+++ index.php
@@ -54,9 +54,9 @@
     <!-- Custom Theme JavaScript -->
     <script src="js/freelancer.js"></script>
 
-    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 
-	    <script src="/<?php echo BASE; ?>estaticos/bootstrap-sweetalert/lib/sweet-alert.min.js"></script> 
+    <script src="/<?php echo BASE; ?>estaticos/bootstrap-sweetalert/lib/sweet-alert.min.js"></script> 
 
-	    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.13.4/jquery.mask.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.13.4/jquery.mask.js'></script>
 </html>
