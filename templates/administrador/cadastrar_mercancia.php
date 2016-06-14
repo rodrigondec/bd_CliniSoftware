fa75fa663c7aaccedcd99bbe5bb332a23d9821e5
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-14 01:44:50 -0300

    cadastrar mercancia funcionando mas não está mapeada no menu.

---
+++ templates/administrador/cadastrar_mercancia.php
@@ -0,0 +1,23 @@
+<div class='text-center'>
+	<h2>Cadastrar Mercância</h2>
+	<hr />
+</div>
+<div class='container col-lg-5 center'>
+	<form method='post'>
+		<div class='form-group col-lg-12'>
+		    <label for='nome'>Nome</label>
+		    <input type='text' name='nome' class='form-control' placeholder='Digite aqui a mercancia' required />
+		</div>
+		<div class='text-center'>
+			<button class='btn btn-danger' type='reset'>Apagar</button>
+            <button class='btn btn-primary' type='submit'>Cadastrar</button>
+		</div>
+	</form>
+</div>
+<?php  
+	if(count($_POST) > 0){
+		insert($_POST, 'mercancia');
+		ob_clean();
+		header('LOCATION: '.ADMINISTRADOR.'/listar_mercancias');
+	}
+?>
