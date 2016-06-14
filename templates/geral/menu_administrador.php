3f30521e3266d9027b68c6e581bf2dc4366329cb
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-14 01:43:39 -0300

    Adicionado mercancia, produto e procedimento ao menu admin

--- templates/geral/menu_administrador.php
+++ templates/geral/menu_administrador.php
@@ -67,7 +67,30 @@
                         <li><a href="<?php echo ADMINISTRADOR; ?>/cadastrar_expediente">Cadastrar</a></li>
                     </ul> 
                 </li>
-
+                <li class='dropdown'>
+                    <a href="#" class='dropdown-toggle' data-toggle='dropdown'>Mercância<span class="caret"></span></a>
+                    <ul class='dropdown-menu'>
+                        <li><a href="<?php echo ADMINISTRADOR; ?>/listar_mercancias">Listar</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-submenu">
+                            <a tabindex="-1" href="#">Produtos<div class='inline'><i class="fa fa-caret-right" style=''></i></div></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo ADMINISTRADOR; ?>/listar_produtos">Listar</a></li>
+                                <li><a href="<?php echo ADMINISTRADOR; ?>/cadastrar_produto">Cadastrar</a></li>
+                            </ul>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-submenu">
+                            <a tabindex="-1" href="#">Procedimentos<div class='inline'><i class="fa fa-caret-right" style=''></i></div></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo ADMINISTRADOR; ?>/listar_procedimentos">Listar</a></li>
+                                <li><a href="<?php echo ADMINISTRADOR; ?>/cadastrar_procedimento">Cadastrar</a></li>
+                                <li><a href="<?php echo ADMINISTRADOR; ?>/selecionar_procedimento_produto">Registrar Inclusão de Produto</a></li>
+                                <li><a href="<?php echo ADMINISTRADOR; ?>/listar_incluidos">Listar Inclusões</a></li>
+                            </ul>
+                        </li>
+                    </ul> 
+                </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">                
                 <li class='dropdown'>
