460115b4f3f16932198d7d269fe492f2aa8526c5
fernandachacon <fernandachaconfontoura@gmail.com>
2016-06-15 20:47:16 -0300

    desmarcar consulta na pasta do paciente

--- templates/paciente/desmarcar_consulta.php
+++ templates/paciente/desmarcar_consulta.php
@@ -1,20 +1,37 @@
 <div class='text-center'>
 	<h2>Desmarcar Consulta</h2>
+	<h6> Consultas Marcadas</h4>
 	<hr />
 </div>
 
-<div class='container col-lg-5 center'>
+<div class='container col-lg-8 center'>
 	<form method='post' >
 		<div class='form-group col-lg-12'>
-		    <label for=''>Consultas Marcadas: </label>
-		    <input type='text' name='nome' class='form-control' placeholder='Digite a especialidade que deseja marcar a consulta'  required />
-		</div>
-		
-		<div class='text-center'>
-			<button class='btn btn-danger' type='reset'>Apagar</button>
-            <button class='btn btn-primary' type='submit'>Buscar médicos</button>
-		</div>
-	</form>
+		  <table class="table table-striped table-hover table-condensed">
+            <thead> 
+                <tr>
+                    <th>Médico</th>
+                    <th>Especialidade</th>
+                    <th>Data</th>
+                    <th>Hora</th>
+                    <th>Observação(ordem de chegada ou horário marcado)</th>
+                </tr>
+                <tbody>
+                <tr>
+            	   <td></td>
+            	   <td></td>
+            	   <td></td>
+            	   <td></td>
+            	   <td></td>
+            	   <td>
+                        <a class='btn btn-danger' data-toggle="modal" >Desmarcar</a>
+                    </td>
+                </tr>
+                </tbody>
+            </thead>
+       	</table> 	
+	</div>	
+</form>
 </div>
 <?php  
 	
