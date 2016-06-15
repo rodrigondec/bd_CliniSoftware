adbe01df4cf3d8b516105b3fa64321c8d1a417b3
fernandachacon <fernandachaconfontoura@gmail.com>
2016-06-15 20:47:50 -0300

    desmarcar consulta na pasta do recepcionista

--- templates/recepcionista/desmarcar_consulta.php
+++ templates/recepcionista/desmarcar_consulta.php
@@ -1,30 +1,40 @@
 <div class='text-center'>
-	<h2>Desmarcar Consulta</h2>
+    <h2>Desmarcar Consulta</h2>
+    <h6> Consultas Marcadas</h4>
+    <hr />
 </div>
 
-<div class="table-responsive container">
-    <table class="table table-striped table-hover table-condensed">
-	<form >
-		<div class='form-group col-lg-12'>
-		    <label for=''>Consultas Marcadas</label>
-		    <br />
-		    <br />
-		 
-		    <thead> 
-            <tr>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>CPF</th>
-                <th>Cadastro Único</th>
-                <th class='col-lg-1'></th>
-                <th class='col-lg-1'></th>
-                
-            </tr>
-        	</thead>
-        	</div>
-     </table>
-		    
-	
-	</form>
+<div class='container col-lg-8 center'>
+    <form method='post' >
+        <div class='form-group col-lg-12'>
+          <table class="table table-striped table-hover table-condensed">
+            <thead> 
+                <tr>
+                    <th>Médico</th>
+                    <th>Especialidade</th>
+                    <th>Data</th>
+                    <th>Hora</th>
+                    <th>Observação(ordem de chegada ou horário marcado)</th>
+                </tr>
+                <tbody>
+                <tr>
+                   <td></td>
+                   <td></td>
+                   <td></td>
+                   <td></td>
+                   <td></td>
+                   <td>
+                        <a class='btn btn-danger' data-toggle="modal" >Desmarcar</a>
+                    </td>
+                </tr>
+                </tbody>
+            </thead>
+        </table>    
+    </div>  
+</form>
 </div>
-        
+<?php  
+    
+
+
+?>
