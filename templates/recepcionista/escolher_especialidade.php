3ad50b000cd3e6e109eeee2ed910db8e67611033
fernandachacon <fernandachaconfontoura@gmail.com>
2016-06-15 20:05:52 -0300

    fazendo o marcar consulta do recepcionista

---
+++ templates/recepcionista/escolher_especialidade.php
@@ -0,0 +1,24 @@
+<div class='text-center'>
+	<h2>Marcar Consulta</h2>
+	<h6> Escolher Especialidade</h6>
+	<hr />
+</div>
+
+<div class='container col-lg-5 center'>
+	<form method='post' action='<?php echo RECEPCIONISTA."/escolher_medico" ?>'>
+		<div class='form-group col-lg-12'>
+		    <label for=''>Especialidade: </label>
+		    <input type='text' name='nome' class='form-control' placeholder='Digite a especialidade que deseja marcar a consulta'  required />
+		</div>
+		
+		<div class='text-center'>
+			<button class='btn btn-danger' type='reset'>Apagar</button>
+            <button class='btn btn-primary' type='submit'>Buscar médicos</button>
+		</div>
+	</form>
+</div>
+<?php  
+	
+
+
+?>
---
+++ templates/recepcionista/escolher_medico.php
@@ -0,0 +1,32 @@
+<div class='text-center'>
+	<h2>Marcar Consulta</h2>
+	<h6> Escolher Médico</h4>
+	<hr />
+</div>
+
+<div class='container col-lg-5 center'>
+	<form method='post' action='<?php echo PACIENTE."/escolher_horario" ?>'>
+		<div class='form-group col-lg-12'>
+		    <label for=''>Médicos com a especialidade escolhida:  </label>
+		 
+		 <br/>
+		 <br/>
+		 <select>
+  			<option value="">Médico 1</option>
+  			<option value="">Médico 2</option>
+  			<option value="">Médico 3</option>
+  			<option value="">Médico 4</option>
+		</select>
+		</div>
+		
+		<div class='text-center'>
+			
+            <button class='btn btn-primary' type='submit'>Buscar horários</button>
+		</div>
+	</form>
+</div>
+<?php  
+	
+
+
+?>
