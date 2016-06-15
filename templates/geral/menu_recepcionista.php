d9f04dad00ebd6e5e2d2a2bb4fcca129283b78d9
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-15 13:38:28 -0300

    recepcionista seleciona usuário para visualizar agenda

--- templates/geral/menu_recepcionista.php
+++ templates/geral/menu_recepcionista.php
@@ -14,7 +14,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div id='navbar' class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="<?php echo RECEPCIONISTA; ?>/selecionar_agenda">Visualizar Agenda</a></li>
+                <li><a href="<?php echo RECEPCIONISTA; ?>/selecionar_usuario">Visualizar Agenda</a></li>
                 <li class='dropdown'>
                     <a href="#" class='dropdown-toggle' data-toggle='dropdown'>Consultas<span class="caret"></span></a>
                     <ul class='dropdown-menu'>
---
+++ templates/recepcionista/selecionar_usuario.php
@@ -0,0 +1,24 @@
+<div class='text-center'>
+	<h2>Selecionar Usuário</h2>
+	<hr />
+</div>
+<div class='container col-lg-5 center'>
+	<form method='post' action='<?php echo SISTEMA."/visualizar_agenda" ?>'>
+		<div class='form-group col-lg-8'>
+		    <label for='nome'>Email</label>
+		    <input type='text' name='email' class='form-control' placeholder='Digite aqui o email' required />
+		</div>
+		<div class="form-group col-lg-4">
+			<label for="sel1">Tipo de usuário</label>
+			<select class="form-control" name='tipo_usuario' required>
+				<option value='' selected disabled>Selecione</option>
+				<option value='paciente'>Paciente</option>
+				<option value='medico'>Médico</option>
+			</select>
+		</div>
+		<div class='text-center'>
+			<button class='btn btn-danger' type='reset'>Apagar</button>
+            <button class='btn btn-primary' type='submit'>Prosseguir</button>
+		</div>
+	</form>
+</div>
