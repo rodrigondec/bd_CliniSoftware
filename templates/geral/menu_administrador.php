7f6a39849b2bfa874e9f3af0da68d9aacef8c010
RodrigoNdeC <rodrigondec@gmail.com>
2016-06-15 21:41:14 -0300

    listar mercancias removido do menu

--- templates/geral/menu_administrador.php
+++ templates/geral/menu_administrador.php
@@ -70,8 +70,6 @@
                 <li class='dropdown'>
                     <a href="#" class='dropdown-toggle' data-toggle='dropdown'>Mercância<span class="caret"></span></a>
                     <ul class='dropdown-menu'>
-                        <li><a href="<?php echo ADMINISTRADOR; ?>/listar_mercancias">Listar</a></li>
-                        <li role="separator" class="divider"></li>
                         <li class="dropdown-submenu">
                             <a tabindex="-1" href="#">Produtos<div class='inline'><i class="fa fa-caret-right" style=''></i></div></a>
                             <ul class="dropdown-menu">
